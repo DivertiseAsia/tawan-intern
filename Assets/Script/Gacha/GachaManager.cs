c09473f5b41e424b4fd878eb2025fb96e2562f3a
Amsun96 <rangsimankt96@gmail.com>
2024-01-19 18:47:09 +0700

    Delete Old Pull Function

--- Assets/Script/Gacha/GachaManager.cs
+++ Assets/Script/Gacha/GachaManager.cs
@@ -10,96 +10,12 @@ public class GachaManager : MonoBehaviour
     [SerializeField] private PlayerStatus _playerStatus;
 
     [Header("Result Panel")]
-    [SerializeField] private GameObject _result_1_Panel;
-    [SerializeField] private GameObject _result_10_Panel;
-    [SerializeField] private Item itemResult;
     [SerializeField] public Item[] itemsResult = new Item[10];
     [SerializeField] ItemScriptableObject[] itemsResultInfo;
 
     [Header("Gacha Config")]
     public int gachaPrice = 100;
     public GachaRate[] gachaRates = new GachaRate[4];
-
-    public void PullOne()
-    {
-        if(_playerStatus.isCurrencyEnough(_bannerManager.bannerType, gachaPrice))
-        {
-            Debug.Log("Start PullOne");
-            int random = Random.Range(1, 101);
-            for (int i = 0; i < gachaRates.Length; i++)
-            {
-                if (random <= gachaRates[i].rate)
-                {
-                    Debug.Log("Randomed Rate " + random);
-                    Debug.Log("Is in Rate of" + gachaRates[i].rate);
-                    Debug.Log("Rarity Is " + gachaRates[i].rarityName);
-
-                    ItemScriptableObject result = GachaResult(gachaRates[i].rarityName);
-                    itemResult.Setup(result);
-
-                    break;
-
-
-                    //Show Gacha
-                }
-            }
-
-            _result_1_Panel.SetActive(true);
-
-            _playerStatus.Pay(_bannerManager.bannerType, gachaPrice);
-            
-            _uiManager.UpdateBannerUI();
-
-            Debug.Log("End PullOne");
-        }
-        else
-        {
-            Debug.Log("Currency is not enough");
-        }
-    }
-
-
-
-    public void PullTen()
-    {
-        if (_playerStatus.isCurrencyEnough(_bannerManager.bannerType, gachaPrice*10))
-        {
-            Debug.Log("Start PullTen");
-
-            foreach (Item item in itemsResult)
-            {
-                int random = Random.Range(1, 101);
-                for (int j = 0; j < gachaRates.Length; j++)
-                {
-                    if (random <= gachaRates[j].rate)
-                    {
-                        Debug.Log("Randomed Rate " + random);
-                        Debug.Log("Is in Rate of" + gachaRates[j].rate);
-                        Debug.Log("Rarity Is " + gachaRates[j].rarityName);
-
-                        ItemScriptableObject result = GachaResult(gachaRates[j].rarityName);
-                        item.Setup(result);
-
-                        break;
-                    }
-                }
-            }
-
-            _result_10_Panel.SetActive(true);
-
-            _playerStatus.Pay(_bannerManager.bannerType, gachaPrice*10);
-            
-            _uiManager.UpdateBannerUI();
-
-            Debug.Log("End PullTen");
-        }
-        else
-        {
-            Debug.Log("Currency is not enough");
-        }
-
-        
-    }
 
     public void Pull(int amount)
     {
